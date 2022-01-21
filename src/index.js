4cd24feee14525912bc24ac22220acd24168b7af
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-01-21 12:28:50 +0200

    towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let arr = []
+  if (matrix === undefined) {
+    return arr
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      arr.push(matrix[i])
+      console.log(arr)
+    } else {
+      arr.push(matrix[i].reverse())
+    }
+  }
+  return [].concat(...arr)
 }
